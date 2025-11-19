12824cca518c6ae61257d64c5b684b1e07d77518
scarletjara <scarlet.jara@hotmail.com>
2025-11-18 23:59:52 -0300

    Se ocultan credenciales

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,9 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        manifestPlaceholders["MAPS_API_KEY"] = project.findProperty("MAPS_API_KEY") ?: ""
+
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
 
